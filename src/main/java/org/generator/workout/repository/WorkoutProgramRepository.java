ab57a5bb571e7997f76751573b1bc24562d6387e
Serg Pretender <sergdynamite@gmail.com>
2025-10-21 21:01:21 +0300

    refactor: modify WorkoutProgramRepository

--- src/main/java/org/generator/workout/repository/WorkoutProgramRepository.java
+++ src/main/java/org/generator/workout/repository/WorkoutProgramRepository.java
@@ -1,7 +1,11 @@
 package org.generator.workout.repository;
 
+import org.generator.workout.model.AppUser;
 import org.generator.workout.model.WorkoutProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutProgramRepository extends JpaRepository<WorkoutProgram, Long> {
+    List<WorkoutProgram> findByUser(AppUser user);
 }
